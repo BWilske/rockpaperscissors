42757a61785135054986703d811a221a66a8f64c
Brent Wilske <wilskeworkflow@gmail.com>
2022-01-16 11:08:05 -0800

    Added gui functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let yourWins = 0
-let computerWins = 0
+let compWins = 0
 
 // evaluate the winner, I feel dumb as shit because there has to be an easier way of doing this, maybe objects when i learn those
 function evalutateWinner(userSelection) {
@@ -10,32 +10,47 @@ function evalutateWinner(userSelection) {
     console.log( `you picked ${userSelection}, computer picked ${computerObject}`)
     
     if (userSelection === computerObject){
-        console.log(`you tied, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+        tie() 
     }
     else if (userSelection === "scissors" && computerObject === "rock") {
-        computerWins+=1
-        console.log(`you lose, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+        computerWins() 
     }
     else if (userSelection === "rock"  && computerObject === "paper"){
-        computerWins+=1
-        console.log(`you lose, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+        computerWins() 
     }
     else if (userSelection === "paper"  && computerObject === "scissors") {
-        computerWins+=1
-        console.log(`you lose, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+        computerWins() 
     }
     else if (userSelection === "scissors" && computerObject === "paper"){
-        yourWins+=1
-        console.log(`you win, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+        playerWins() 
     }
     else if (userSelection === "rock" && computerObject === "scissors"){
-        yourWins+=1
-        console.log(`you win, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+        playerWins() 
     }
     else if (userSelection === "paper" && computerObject === "rock") {
-        yourWins+=1
-        console.log(`you win, Your wins: ${yourWins}   Computer wins: ${computerWins}`) 
+       playerWins() 
     }
+}
+
+//helper functions
+
+const playerWins = () => {
+    yourWins+=1
+    yourScoreEl.innerHTML =`<h5>${yourWins}</h5>`
+    winOrLoseEl.innerHTML ="<h3>YOU WIN<H3>"
+    console.log(`you win, Your wins: ${yourWins}   Computer wins: ${compWins}`)
+}
+
+const computerWins = () => {
+    compWins+=1
+    compScoreEl.innerHTML = `<h5>${compWins}</h5>`
+    winOrLoseEl.innerHTML ="<h3>YOU LOSE<H3>"
+    console.log(`you lose, Your wins: ${yourWins}   Computer wins: ${compWins}`)
+}
+const tie = () => {
+    console.log(`you tied, Your wins: ${yourWins}   Computer wins: ${compWins}`)
+    winOrLoseEl.innerHTML ="<h3>TIE<H3>"
+
 }
 
 //event listens for buttons and such
@@ -43,3 +58,6 @@ function evalutateWinner(userSelection) {
 let rockEl = document.getElementById("rockbutton").addEventListener("click", () => evalutateWinner("rock"))
 let paperEl= document.getElementById("paperbutton").addEventListener("click", () => evalutateWinner("paper"))
 let scissorsEl= document.getElementById("scissorsbutton").addEventListener("click", () => evalutateWinner("scissors"))
+let yourScoreEl = document.getElementById("yourscore")
+let compScoreEl = document.getElementById("compscore")
+let winOrLoseEl = document.getElementById("winorlost")
